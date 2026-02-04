5a00bf211d500543a19b5935cefefcb2d0995b87
TarekAlkhen <alkhentarek9@gmail.com>
2026-02-04 18:58:37 -0500

    Update frontend/main.js
    Co-authored-by: FM <29071567+fernando-mf@users.noreply.github.com>

--- frontend/main.js
+++ frontend/main.js
@@ -20,7 +20,7 @@ function isPasswordValid(password){
 
 /* ---------------- REGISTER (US.02) ---------------- */
 async function registerRequest(firstName, lastName, email, password){
-  const response = await fetch("http://localhost:3000/register", {
+  const response = await fetch(`${BASE_URL}/register`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ firstName, lastName, email, password })

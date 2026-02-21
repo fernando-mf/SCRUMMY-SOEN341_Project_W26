cc60bc48cf0a2bd3cae305b1461f05b3ce1e9709
Pizzaboy2508 <glmarino04@icloud.com>
2026-02-20 22:19:00 -0500

    Added recipes framework and recipe creation model

---
+++ backend/src/recipes/postgres_repository.ts
@@ -0,0 +1,12 @@
+import postgres from "postgres";
+import { IRecipesRepository, Recipe } from "./recipes";
+
+export class RecipesRepository implements IRecipesRepository {
+    constructor(private db: postgres.Sql) {}
+ 
+    async Create(recipe: Omit<Recipe, "id">): Promise<Recipe> {
+        //TODO
+        throw new Error("Method not implemented.");
+    }
+    
+}
---
+++ backend/src/recipes/recipes.ts
@@ -0,0 +1,64 @@
+import { z } from "zod";
+
+export enum Unit {
+    G = "g",
+    ML = "ml",
+    TBSP = "tbsp",
+    TSP = "tsp",
+};
+
+export enum Difficulty {
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard",
+};
+
+export type Ingredient = {
+    name: string;
+    amount: number;
+    unit: Unit;
+};
+
+export type Recipe = {
+    id: number;
+    authorID: number;
+    ingredients: Ingredient[];
+    prepTime: number;
+    prepSteps: string;
+    cost: number;
+    difficulty: Difficulty;
+    dietaryTags: string[];
+    allergens: string;
+    servings?: number;
+};
+
+//Response Types
+export type CreateRecipeResponse = {
+    recipe: Recipe;
+};
+
+
+//Request Schemas
+const createRecipeRequestSchema = z.object({
+    //TODO
+});
+
+export type CreateRecipeRequest = z.infer<typeof createRecipeRequestSchema>;
+
+//Interfaces
+export interface IRecipesService {
+    Create(authorID: number, request: CreateRecipeRequest): Promise<CreateRecipeResponse>;
+}
+
+export interface IRecipesRepository {
+    Create(recipe: Omit<Recipe, "id">): Promise<Recipe>;
+}
+
+export class RecipesService implements IRecipesService {
+    constructor(private repository: IRecipesRepository) {}
+
+    async Create(authorID: number, request: CreateRecipeRequest): Promise<CreateRecipeResponse> {
+        throw new Error("Method not implemented.");
+    }
+    
+}
